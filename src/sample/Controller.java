cb076c3e0b17df1d854485db17147e21c4f591b9
rafaromey <allgafar@hotmail.com>
2020-05-16 20:10:44 +0200

    Created method to insert into DB customer values. and Replaced sqljdc4 jar file into project folder

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -3,6 +3,6 @@ package sample;
 public class Controller {
 
     public static void main(String[] args) {
-       // testtest
+
     }
 }
--- src/sample/Customer.java
+++ src/sample/Customer.java
@@ -1,4 +1,54 @@
 package sample;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
 public class Customer {
+    private static Connection con;
+    private static String port;
+    private static String databaseName;
+    private static String userName;
+    private static String password;
+
+    static {
+        Properties props = new Properties();
+        String fileName = "db.properties";
+        InputStream input;
+        try {
+            input = new FileInputStream(fileName);
+            props.load(input);
+            port = props.getProperty("port", "1433");
+            databaseName = props.getProperty("databaseName");
+            userName = props.getProperty("userName", "sa");
+            password = props.getProperty("password");
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            System.out.println("Database Ready");
+
+        } catch (IOException | ClassNotFoundException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public void createCustomer(String customerName, String mail, int phoneNO) {
+        try {
+            con = DriverManager.getConnection("jdbc:sqlserver://localhost:" + port + ";databaseName=" + databaseName, userName, password);  // to hide the password in file.
+            Statement stmt = con.createStatement();
+
+
+            stmt.executeUpdate("insert into tblCustomer (fldName ,fldMail, fldPhoneNo) values('"+ customerName+ "','" + mail +"' , " + phoneNO+")");
+
+
+            con.close();
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+
+
+    }
 }
