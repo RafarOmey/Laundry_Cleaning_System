a7f00168d5d6d372bc3d60ab69eeafd0320da8e9
rafaromey <allgafar@hotmail.com>
2020-05-16 20:44:15 +0200

    Merg

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,24 +1,39 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
 public class Controller {
-
     @FXML
-    private TextField orderCustomerID,deliveryPointID;
+    TextField textCustomerName, orderCustomerID, deliveryPointID, textCustomerPhoneNO, textCustomerMail;
+    @FXML
+    Button buttonCreateCustomer;
 
 
+    public void createCustomer() {
+        Customer createNewCustomer = new Customer();
+        String customerName = (textCustomerName.getText());
+        String mail = (textCustomerMail.getText());
+        int phoneNO = Integer.parseInt(textCustomerPhoneNO.getText());
 
 
+        createNewCustomer.insertTnToCustomer(customerName, mail, phoneNO);
 
-    public void createOrder(){
+    }
+
+
+    public void createOrder() {
         int customerID = Integer.parseInt(orderCustomerID.getText());
         int deliveryPoint = Integer.parseInt(deliveryPointID.getText());
 
 
         Order order = new Order();
-        order.createOrder(customerID,deliveryPoint);
+        order.createOrder(customerID, deliveryPoint);
 
     }
 }
